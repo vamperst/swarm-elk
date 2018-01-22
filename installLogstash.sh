ef6ddfee5589f8ba5af5f1d02ef882d83a0bec39
Rafael Barbosa <vamperst@gmail.com>
2018-01-22 16:22:17 -0200

    logstash local

---
+++ installLogstash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update -y
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https -y 
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo apt-get update -y 
+sudo apt-get install logstash -y
+
+sudo cp logstash/logstash.yml /usr/share/logstash/config/logstash.yml
+service logstash restart
+
